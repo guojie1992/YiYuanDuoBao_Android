08047839453d4bb24f756957f857c82d0a96b9bf
chung567115 <1173572995@qq.com>
2016-08-09 20:17:21 +0800

    20160809 基础版

---
+++ app/src/main/java/so/len/duobao/customAdapter/TreasureGridViewAdapter.java
@@ -0,0 +1,85 @@
+package so.len.duobao.customAdapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.rey.material.widget.ProgressView;
+
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import so.len.duobao.R;
+
+/**
+ * Created by Chung on 2016/8/9.
+ */
+public class TreasureGridViewAdapter extends BaseAdapter {
+    private Context context;
+    private List<Map<String, Object>> data;
+    private LayoutInflater layoutInflater;
+
+    public TreasureGridViewAdapter(Context context, List<Map<String, Object>> data) {
+        this.context = context;
+        this.data = data;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.item_gridview_treasure, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.ivTitleItemGridviewTreasure.setImageResource((Integer) data.get(position).get("ivTitleItemGridviewTreasure"));
+        viewHolder.tvTitleItemGridviewTreasure.setText((CharSequence) data.get(position).get("tvTitleItemGridviewTreasure"));
+        viewHolder.pvProgressItemGridviewTreasure.setProgress((float) 0.5);//.setProgress((Float) data.get(position).get("pvProgressItemGridviewTreasure"));
+        viewHolder.tvAllItemGridviewTreasure.setText((CharSequence) data.get(position).get("tvAllItemGridviewTreasure"));
+        viewHolder.tvNeedItemGridviewTreasure.setText((CharSequence) data.get(position).get("tvNeedItemGridviewTreasure"));
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.iv_title_item_gridview_treasure)
+        ImageView ivTitleItemGridviewTreasure;
+        @BindView(R.id.tv_title_item_gridview_treasure)
+        TextView tvTitleItemGridviewTreasure;
+        @BindView(R.id.pv_progress_item_gridview_treasure)
+        ProgressView pvProgressItemGridviewTreasure;
+        @BindView(R.id.tv_all_item_gridview_treasure)
+        TextView tvAllItemGridviewTreasure;
+        @BindView(R.id.tv_need_item_gridview_treasure)
+        TextView tvNeedItemGridviewTreasure;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/so/len/duobao/fragment/FourFragment.java
+++ app/src/main/java/so/len/duobao/fragment/FourFragment.java
@@ -5,23 +5,38 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import so.len.duobao.R;
+import so.len.duobao.customAdapter.LotteryListViewAdapter;
+import so.len.duobao.customAdapter.TreasureGridViewAdapter;
 import so.len.duobao.iPresenter.FourPresenter;
 import so.len.duobao.iView.IFourView;
 
 /**
  * Created by Chung on 2016/8/3.
  */
-public class FourFragment extends BaseFragment implements IFourView{
-
+public class FourFragment extends BaseFragment implements IFourView {
+    @BindView(R.id.gv_treasure_fragment_four)
+    GridView gvTreasureFragmentFour;
 
     private FourPresenter fourPresenter;
+    private List<Map<String, Object>> treasureListData;
+    private HashMap<String, Object> map;
+    private TreasureGridViewAdapter treasureGridViewAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_four, null);
-
+        ButterKnife.bind(this, view);
 
         control();
         return view;
@@ -34,6 +49,18 @@ public class FourFragment extends BaseFragment implements IFourView{
 
     @Override
     public void initTreasureList() {
-
+        treasureListData = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            map = new HashMap<>();
+            map.put("ivTitleItemGridviewTreasure", R.drawable.title);
+            map.put("tvTitleItemGridviewTreasure", "IPhone 6s Plus 128GB");
+            map.put("pvProgressItemGridviewTreasure", 55.0);
+            map.put("tvAllItemGridviewTreasure", "2222");
+            map.put("tvNeedItemGridviewTreasure", "1111");
+            treasureListData.add(map);
+        }
+        logInfo(treasureListData.toString());
+        treasureGridViewAdapter = new TreasureGridViewAdapter(getActivity(), treasureListData);
+        gvTreasureFragmentFour.setAdapter(treasureGridViewAdapter);
     }
 }
